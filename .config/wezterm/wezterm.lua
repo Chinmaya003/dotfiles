16e347372475791e59d658b666f1b3bf5d57617a
Chinmaya S C <120659062+Chinmaya003@users.noreply.github.com>
2025-02-17 17:34:04 +0530

    Add files via upload

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,57 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font_size = 13
+config.font = wezterm.font("Iosevka Nerd Font Mono")
+config.automatically_reload_config = true
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.adjust_window_size_when_changing_font_size = false
+config.harfbuzz_features = { "calt=0" }
+config.max_fps = 120
+config.animation_fps = 120
+local gpus = wezterm.gui.enumerate_gpus()
+config.webgpu_preferred_adapter = gpus[1]
+config.front_end = 'WebGpu'
+config.enable_kitty_graphics = true
+config.window_close_confirmation = "NeverPrompt"
+config.window_background_opacity = 0.67
+config.macos_window_background_blur = 12
+config.audible_bell = "Disabled"
+webgpu_force_fallback_adapter = true
+
+
+config.window_padding = {
+	left = 18,
+	right = 15,
+	top = 20,
+	bottom = 5,
+}
+
+-- Key bindings delete word
+config.keys = {
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = wezterm.action({ SendString = "\x1bb" }),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = wezterm.action({ SendString = "\x1bf" }),
+	},
+}
+
+-- Configs
+-- Add Custom Color Scheme
+config.color_scheme = "Count Von Count (terminal.sexy)"
+config.colors = {
+	-- background = "#12151B", -- lighter gray
+	-- background = "#0D0D0D", -- darker gray
+	background = "#000", -- pure black
+	cursor_bg = "#9B96B5",
+	cursor_fg = "#1a1a1e",
+	cursor_border = "#9B96B5",
+}
+
+return config
